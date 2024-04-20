f6412491528e63fc2f615cb6127f39ac08a572c1
olaf <125729313+spinningrat69@users.noreply.github.com>
2024-04-20 18:01:42 +0300

    add backend for the down syndrome website
    title

---
+++ script.js
@@ -0,0 +1,32 @@
+let count = 0;
+let cooldown = false;
+
+
+async function getSigmaCounter() {
+  const response = await fetch("https://api.down-syndro.me:42069/api/count");
+  const text = await response.text();
+  console.log(text);
+  
+}
+
+setInterval(getSigmaCounter, 2500);
+
+async function increaseSigmaCounter() {
+    if (!cooldown) {
+        increaseCounter();
+        let audio = document.getElementById("sigmaSound");
+        audio.currentTime = 0;
+        audio.play();
+        count++;
+        document.getElementById("count").textContent = count;
+        cooldown = true;
+        setTimeout(() => {
+            cooldown = false;
+        }, 2500);
+        
+        fetch("https://api.down-syndro.me:42069/api/count", {
+            method: "POST",
+        })
+    }
+}
+      
